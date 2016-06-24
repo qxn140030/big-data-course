2c7f2bd19dd4035d912f457e1b049bbeb8a55777
Qun Niu <qun.niu321@gmail.com>
2016-06-24 09:22:07 -0500

    Update HW1-Q2.java

--- HW1-Q2.java
+++ HW1-Q2.java
@@ -12,6 +12,10 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.StringUtils;
+
+/*
+ * Given any two Users as input, output the list of the user id of their mutual friends.
+ */
  
 public class Q2 {
 	
@@ -21,26 +25,26 @@ public class Q2 {
         private Text m_friends = new Text();
         
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-        	//use Context object get conf object
+            //use Context object get conf object
             Configuration conf = context. getConfiguration();
-            String arg0 = conf.get("leftKey");   // e.g. arg0 = 0
-            String arg1 = conf.get("rightKey");  // e.g. arg1 = 1
+            String arg0 = conf.get("leftKey");      // e.g. arg0 = 0
+            String arg1 = conf.get("rightKey");     // e.g. arg1 = 1
           
             // the key is null and the value is one line of our input file.
             // Split by \t to separate the user and its friends list.
-            String line = value.toString();  // e.g. line = 0	1,2   
+            String line = value.toString();         // e.g. line = 0	1,2   
             String[] userAndFriends = line.split("\t");
-            String user = userAndFriends[0];  // e.g. user = 0
-            String id = arg0.compareTo(arg1) < 0 ? arg0 + "," + arg1 : arg1 + "," + arg0;  // e.g. id = (0,1) (0,2)
+            String user = userAndFriends[0];        // e.g. user = 0
+            String id = arg0.compareTo(arg1) < 0 ? arg0 + "," + arg1 : arg1 + "," + arg0;    // e.g. id = (0,1) (0,2)
         	m_id.set(id);
-            if(userAndFriends.length == 1 && (user.equals(arg0) || user.equals(arg1))) {
+            if (userAndFriends.length == 1 && (user.equals(arg0) || user.equals(arg1))) {
             	context.write(m_id, new Text());
             }
-            if(userAndFriends.length == 2 && (user.equals(arg0) || user.equals(arg1))) {
-	            String friends = userAndFriends[1];  // e.g. friends = {1,2}
-	        	// For each friend in the list, output the (UserFriend, ListOfFriends) pair
-	        	m_friends.set(friends);  // e.g. m_friends = (1,2)
-	        	context.write(m_id, m_friends);  // e.g. key = (0, 1)  value = (1,2)                
+            if (userAndFriends.length == 2 && (user.equals(arg0) || user.equals(arg1))) {
+	        String friends = userAndFriends[1];     // e.g. friends = {1,2}
+	        // For each friend in the list, output the (UserFriend, ListOfFriends) pair
+	        m_friends.set(friends);                 // e.g. m_friends = (1,2)
+	        context.write(m_id, m_friends);         // e.g. key = (0, 1)  value = (1,2)                
             }	            
         }
     }
@@ -52,13 +56,13 @@ public class Q2 {
             // Prepare a 2-String-Array to hold the values, i.e. the friends lists of our current friends pair.
             String[] preCombine = new String[2];
             int count = 0;
-            for(Text value : values) {
+            for (Text value : values) {
             	preCombine[count++] = value.toString();
             }
  
             // Calculate the intersection of these lists and write result in the form (UserAUserB, MutualFriends).
             String r_str = intersection(preCombine[0], preCombine[1]);
-            if(r_str != null) {
+            if (r_str != null) {
             	r_result.set(r_str);
             } else {
             	r_result.set(" ");
